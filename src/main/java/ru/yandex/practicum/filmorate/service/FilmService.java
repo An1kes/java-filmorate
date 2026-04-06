9c06ac9635ad425e95a13a50aee2cd0720bc7a77
Vlad <kichatov.vlad@yandex.ru>
2026-04-07 02:28:14 +0500

    Финальное задание. Спринт 11. CheckStyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.util.Collection;
@@ -14,7 +13,6 @@ public class FilmService {
 
     private static final int DEFAULT_POPULAR_COUNT = 10;
     private final InMemoryFilmStorage filmStorage;
-    //private final FilmStorage filmStorage;
 
 
     public Film createFilm(Film film) {
